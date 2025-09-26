ad04323afb601b8c54e1709b7965ae90eafaf578
VitorioSilva <vitorio.tech@proton.me>
2025-09-26 01:15:02 -0300

    feat: Adicionei todas as senções de filmes e serie e imagens iniciais

--- data/movies.ts
+++ data/movies.ts
@@ -1,9 +1,101 @@
-// data/movies.ts
 export const movieSections = [
   {
-    title: "Netflix Originals",
+    title: "Releases in the Past Year",
     data: [
       { id: "1", poster: require("../assets/filmes/moneyheist.png") },
+      { id: "2", poster: require("../assets/filmes/strangerthings.png") },
+      { id: "3", poster: require("../assets/filmes/peakyblinders.png") },
+      { id: "4", poster: require("../assets/filmes/witcher.png") },
+      { id: "5", poster: require("../assets/filmes/squidgame.png") },
+      { id: "6", poster: require("../assets/filmes/lupin.png") },
     ]
-  }
-]
+    },
+  {
+    title: "Continue Watcning for Drashti",
+    data: [
+      { id: "7", poster: require("../assets/images/netflix/") },
+      { id: "8", poster: require("../assets/images/netflix/") },
+      { id: "9", poster: require("../assets/images/netflix/") },
+      { id: "10", poster: require("../assets/images/netflix/") },
+      { id: "11", poster: require("../assets/images/netflix/") },
+      { id: "12", poster: require("../assets/images/netflix/") },
+    ]
+  },
+  {
+    title: "Suspenseful TV Shows",
+    data: [
+      { id: "13", poster: require("../assets/images/netflix/moneyheist.png") },
+      { id: "14", poster: require("../assets/images/netflix/strangerthings.png") },
+      { id: "15", poster: require("../assets/images/netflix/peakyblinders.png") },
+      { id: "16", poster: require("../assets/images/netflix/") },
+      { id: "17", poster: require("../assets/images/netflix/") },
+      { id: "18", poster: require("../assets/images/netflix/") },
+    ]
+  },
+  {
+    title: "Select for You Today",
+    data: [
+      { id: "19", poster: require("../assets/images/netflix/") },
+      { id: "20", poster: require("../assets/images/netflix/") },
+      { id: "21", poster: require("../assets/images/netflix/") },
+      { id: "22", poster: require("../assets/images/netflix/") },
+      { id: "23", poster: require("../assets/images/netflix/") },
+      { id: "24", poster: require("../assets/images/netflix/") },
+    ]
+  },
+    {
+    title: "My List",
+    data: [
+      { id: "25", poster: require("../assets/images/netflix/") },
+      { id: "26", poster: require("../assets/images/netflix/") },
+      { id: "27", poster: require("../assets/images/netflix/") },
+      { id: "28", poster: require("../assets/images/netflix/") },
+      { id: "29", poster: require("../assets/images/netflix/") },
+      { id: "30", poster: require("../assets/images/netflix/") },
+    ]
+  },
+    {
+    title: "New Releases",
+    data: [
+      { id: "31", poster: require("../assets/images/netflix/") },
+      { id: "32", poster: require("../assets/images/netflix/") },
+      { id: "33", poster: require("../assets/images/netflix/") },
+      { id: "34", poster: require("../assets/images/netflix/") },
+      { id: "35", poster: require("../assets/images/netflix/") },
+      { id: "36", poster: require("../assets/images/netflix/") },
+    ]
+  },
+    {
+    title: "Ensemble TV Shows",
+    data: [
+      { id: "37", poster: require("../assets/images/netflix/") },
+      { id: "38", poster: require("../assets/images/netflix/") },
+      { id: "39", poster: require("../assets/images/netflix/") },
+      { id: "40", poster: require("../assets/images/netflix/") },
+      { id: "41", poster: require("../assets/images/netflix/") },
+      { id: "42", poster: require("../assets/images/netflix/") },
+    ]
+  },
+    {
+    title: "Chilly Thrillers",
+    data: [
+      { id: "43", poster: require("../assets/images/netflix/lupin.png") },
+      { id: "44", poster: require("../assets/images/netflix/") },
+      { id: "45", poster: require("../assets/images/netflix/") },
+      { id: "46", poster: require("../assets/images/netflix/") },
+      { id: "47", poster: require("../assets/images/netflix/") },
+      { id: "48", poster: require("../assets/images/netflix/") },
+    ]
+  },
+    {
+    title: "Only on Netflix",
+    data: [
+      { id: "49", poster: require("../assets/images/netflix/") },
+      { id: "50", poster: require("../assets/images/netflix/") },
+      { id: "51", poster: require("../assets/images/netflix/") },
+      { id: "52", poster: require("../assets/images/netflix/") },
+      { id: "53", poster: require("../assets/images/netflix/") },
+      { id: "54", poster: require("../assets/images/netflix/") },
+    ]
+  },
+];
